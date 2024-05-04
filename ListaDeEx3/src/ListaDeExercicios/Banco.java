812f0354848f6d66325c61eb52920179a6fb0a81
weverttongg <weverttonrodrigues09@gmail.com>
2024-05-04 16:22:13 -0300

    ATUALIZAÇÃO DO CODIGO

---
+++ ListaDeEx3/src/ListaDeExercicios/Banco.java
@@ -0,0 +1,47 @@
+
+package ListaDeExercicios;
+
+
+public class Banco {
+    //variaveis
+    private int codigo;
+    private String nome;
+    private String paisOrgigem;
+    
+                //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL CODIGO ( metodo que retorna um valor e sem parâmetro )
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    // (Set) VARIAVEL CODIGO ( metodo que não retorna valor e com parâmetro ) 
+    public void setCodigo (int codigo) {
+       this.codigo = codigo;
+    }
+    
+                //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL NOME ( metodo que retorna um valor e sem parâmetro )
+    public String getNome () {
+        return nome;
+    }
+    
+    // (Set) VARIAVEL NOME ( metodo que não retorna valor e com parâmetro ) 
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+    
+                    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL PAISORIGEM ( metodo que retorna um valor e sem parâmetro )
+    public String getPaisOrgiem () {
+        return paisOrgigem;
+    }
+    
+    // (Set) VARIAVEL PAISORIGEM ( metodo que não retorna valor e com parâmetro ) 
+    public void setPaisOrigem(String paisOrigem) {
+        this.paisOrgigem = paisOrigem;
+    }
+    
+}
