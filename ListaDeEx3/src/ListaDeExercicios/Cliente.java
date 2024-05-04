a6132853fad9772858f94fd1077c72b8b43ba076
weverttongg <weverttonrodrigues09@gmail.com>
2024-05-04 16:51:36 -0300

    caracteristicas do cliente

--- ListaDeEx3/src/ListaDeExercicios/Cliente.java
+++ ListaDeEx3/src/ListaDeExercicios/Cliente.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ListaDeExercicios;
 
-/**
- *
- * @author wever
- */
 public class Cliente {
+   //variaveis no estado de PRIVADAS
+private String nome;
+private String identidade;
+private String cpf;
+
+                //ENCAPSULAMENTO
     
+    // (Get) VARIAVEL NOME ( metodo que retorna um valor e sem parâmetro )
+    public String getNome(){
+        return nome;
+    }
+    
+// (Set) VARIAVEL NOME ( metodo que não retorna valor e com parâmetro ) 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+                //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL IDENTIDADE ( metodo que retorna um valor e sem parâmetro )
+    public String getIdentidade() {
+            return identidade;
+    }
+    
+// (Set) VARIAVEL IDENTIDADE ( metodo que não retorna valor e com parâmetro ) 
+    public void setIdentidade(String identidade) {
+        this.identidade = identidade;
+    }
+    
+    
+                //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL NOME ( metodo que retorna um valor e sem parâmetro )
+// (Set) VARIAVEL NOME ( metodo que não retorna valor e com parâmetro ) 
+
+                //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL NOME ( metodo que retorna um valor e sem parâmetro )
+// (Set) VARIAVEL NOME ( metodo que não retorna valor e com parâmetro ) 
+
+
+
 }
