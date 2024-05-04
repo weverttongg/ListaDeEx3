d0e23ab60c4f9e3f96c490482ae92d0d90cc894b
weverttongg <weverttonrodrigues09@gmail.com>
2024-05-04 17:03:29 -0300

    Update TESTE

--- ListaDeEx3/src/ListaDeExercicios/TESTE.java
+++ ListaDeEx3/src/ListaDeExercicios/TESTE.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ListaDeExercicios;
 
-/**
- *
- * @author wever
- */
 public class TESTE {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
     
 }
