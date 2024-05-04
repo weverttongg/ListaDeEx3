99914eaf9626ed16d4fc2b1c077b8e3dbe8b0bad
weverttongg <weverttonrodrigues09@gmail.com>
2024-05-04 16:41:08 -0300

    Caracteristicas do Endereço

--- ListaDeEx3/src/ListaDeExercicios/Endereco.java
+++ ListaDeEx3/src/ListaDeExercicios/Endereco.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ListaDeExercicios;
 
-/**
- *
- * @author wever
- */
 public class Endereco {
+    //variaveis no estado de PRIVADAS
+    private String rua;
+    private int numero;
+    private String telefone;
     
+                    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL RUA ( metodo que retorna um valor e sem par?metro )
+    public String getRua() {
+        return rua;
+    }
+    
+    // (Set) VARIAVEL RUA ( metodo que n?o retorna valor e com par?metro ) 
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    
+                    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL NUMERO ( metodo que retorna um valor e sem par?metro )
+    public int getNumero() {
+        return numero;
+    }
+    
+    // (Set) VARIAVEL NUMERO ( metodo que n?o retorna valor e com par?metro )
+    public void setNumero (int numero) {
+        this.numero = numero;
+    }
+                    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL TELEFONE ( metodo que retorna um valor e sem par?metro )
+    public String getTelefone() {
+        return telefone;
+    }
+    
+    // (Set) VARIAVEL TELEFONE ( metodo que n?o retorna valor e com par?metro ) 
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
