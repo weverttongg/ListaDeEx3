a04473f98369911cab78b2d6af805d708c78a53f
weverttongg <weverttonrodrigues09@gmail.com>
2024-05-04 16:32:00 -0300

    Caracteristicas da Conta Corrente

--- ListaDeEx3/src/ListaDeExercicios/ContaCorrente.java
+++ ListaDeEx3/src/ListaDeExercicios/ContaCorrente.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ListaDeExercicios;
 
-/**
- *
- * @author wever
- */
 public class ContaCorrente {
+    //variaveis no estado de PRIVADAS
+    private int numero;
+    private int tipo;
+    private double saldo;
+    
+    
+                    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL NUMERO ( metodo que retorna um valor e sem parâmetro )
+    public int getNumero() {
+        return numero;
+    }
+    
+    // (Set) VARIAVEL NUMERO ( metodo que não retorna valor e com parâmetro ) 
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+           
+                   //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL TIPO ( metodo que retorna um valor e sem parâmetro )
+    public int getTipo () {
+        return tipo;
+    }
+    
+    // (Set) VARIAVEL TIPO ( metodo que não retorna valor e com parâmetro ) 
+    public void setTipo (int tipo) {
+        this.tipo = tipo;
+    }
+    
+                    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL SALDO ( metodo que retorna um valor e sem parâmetro )
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    // (Set) VARIAVEL SALDO ( metodo que não retorna valor e com parâmetro ) 
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
     
 }
