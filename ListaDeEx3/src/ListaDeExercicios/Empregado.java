b5c86355185945fcc2afda29ce02023cc5d9cd77
weverttongg <weverttonrodrigues09@gmail.com>
2024-05-04 17:25:42 -0300

    Caracteristicas do Empregado

--- ListaDeEx3/src/ListaDeExercicios/Empregado.java
+++ ListaDeEx3/src/ListaDeExercicios/Empregado.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ListaDeExercicios;
 
-/**
- *
- * @author wever
- */
 public class Empregado {
+    //variaveis no estado de PRIVADAS
+    private String nome;
+    private int matricula;
+    private String funcao;
+    
+    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL NOME ( metodo que retorna um valor e sem parâmetro )
+    public String getNome() {
+        return nome;
+    }
+    
+// (Set) VARIAVEL NOME ( metodo que não retorna valor e com parâmetro ) 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL MATRICULA ( metodo que retorna um valor e sem parâmetro )
+    public int getMatricula() {
+        return matricula;
+    }
+    
+// (Set) VARIAVEL MATRICULA ( metodo que não retorna valor e com parâmetro ) 
+    public void setMatricula(int matricula){
+        this.matricula = matricula;
+    }
+    
+    //ENCAPSULAMENTO
+    
+    // (Get) VARIAVEL FUNCAO ( metodo que retorna um valor e sem parâmetro )
+    public String getFuncao() {
+        return funcao;
+    }
+    
+// (Set) VARIAVEL FUNCAO ( metodo que não retorna valor e com parâmetro ) 
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+    
+    //METÓDO ToString
+    @Override
+    public String toString() {
+        return "Empregado{" + "nome=" + nome + ", matricula=" + matricula + ", funcao=" + funcao + '}';
+    }
+    
+    //METÓDO CONSTRUCTOR
+    public Empregado(String nome, int matricula, String funcao) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.funcao = funcao;
+    }
     
 }
